057019b94fa0e473e7ea44b8d1b9a309d2df00d9
MichaelTabb <mtabbinnor@gmail.com>
2024-01-18 13:19:26 +0000

    commit

--- task2.py
+++ task2.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from common.utils import create_webhook
 from webexteamssdk import WebexTeamsAPI, Webhook
 
-WEBEX_TEAMS_ACCESS_TOKEN = 'NjRlZjdiMTYtNTRiMy00NjNhLTkzZWQtMDQ2MjFmNmQzZGNiM2VjYTkxNDgtZGZj_P0A1_19365916-598f-457d-9ca7-51a422c8769e'
+WEBEX_TEAMS_ACCESS_TOKEN = 'OGFmMDE0ZGItZDQyYS00YTJmLWFkMDYtMDA5ZWZkNzE3N2U2OTg2MDdkOWMtMTI3_P0A1_19365916-598f-457d-9ca7-51a422c8769e'
 
 teams_api = None
 all_polls = {}
