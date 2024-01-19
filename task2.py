1e1b03d8b10bca688999c95e5a9d9a41bba32884
MichaelTabb <mtabbinnor@gmail.com>
2024-01-19 13:35:06 +0000

    commit

--- task2.py
+++ task2.py
@@ -43,15 +43,7 @@ def parse_message(command, sender, roomId):
             end_poll(roomId, sender)
     elif command == "help":
         if all_polls[roomId]:
-            send_message_in_room(roomId, "These are all of the commands you can use:\n"
-                                 "\n"
-                                 "add option: Adds a poll option that you can choose from once you have created the poll\n"
-                                 "create poll: Creates a poll to use\n"
-                                 "end poll: Ends the poll and displays the results\n"
-                                 "help: Gives the list of active commands that can be used\n"
-                                 "ping poll: pings a reminder to anybody who hasn't voted in the poll\n"
-                                 "status poll: Gives the satus of the poll and whether it has started and if there are any results"
-                                 "start poll: starts the poll and allows for users to vote in the poll")
+            send_message_in_room(roomId, "These are all of the commands you can use:")
     return
 
 def generate_start_poll_card(roomId):
